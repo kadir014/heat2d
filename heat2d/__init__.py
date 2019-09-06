2501592b369574191f10b6a63d6c59d136e643eb
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2019-09-07 00:25:38 +0300

    Fix import namespace

--- heat2d/__init__.py
+++ heat2d/__init__.py
@@ -5,9 +5,9 @@ state = "Alpha"
 author = "Kadir Aksoy"
 license = "GNU General Public License v3.0"
 
-from heat2d_dev.engine import Engine
-from heat2d_dev.entity import Entity
-from heat2d_dev.stage import Stage
-from heat2d_dev.sprite import Sprite
+from heat2d.engine import Engine
+from heat2d.entity import Entity
+from heat2d.stage import Stage
+from heat2d.sprite import Sprite
 
-from heat2d_dev.gui.label import Label
+from heat2d.gui.label import Label
