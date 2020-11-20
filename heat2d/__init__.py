2d57df6265480013a4e967761429dd440f6de59d
Kadir Aksoy <kursatkadir014@gmail.com>
2020-11-20 22:15:01 +0300

    0.0.4

--- heat2d/__init__.py
+++ heat2d/__init__.py
@@ -1,54 +1,59 @@
-"""
-      HEAT2D Game Engine
-     GPL 3.0 © Kadir Aksoy
-https://github.com/kadir014/heat2d
-
-"""
-
-VERSION =       "0.0.3"
-VERSION_TUPLE = (0, 0, 3)
-VERSION_STATE = "alpha"
-LICENSE =       "GNU General Public License v3.0"
-
-import sys
-PLATFORM = None
-if   sys.platform == "win32": PLATFORM = "Windows"
-elif sys.platform == "darwin":  PLATFORM = "MacOS"
-elif sys.platform.startswith("linux"): PLATFORM = "Linux"
-elif sys.platform.startswith("freebsd"): PLATFORM = "FreeBSD"
+#       HEAT2D Game Engine
+#      GPL 3.0 © Kadir Aksoy
+#https://github.com/kadir014/heat2d
 
 import os
+
+from heat2d.libs import utils
+
+from heat2d.libs import specs
+specs.OS.update_info()
+if "HEAT2D_UPDATE_SPECS_INFO" not in os.environ: os.environ["HEAT2D_UPDATE_SPECS_INFO"] = "1"
+if os.environ["HEAT2D_UPDATE_SPECS_INFO"] == "1":
+    print("You can see all by using 'heat2d.specs.print_specs()'\n")
+    print("Gathering RAM information...")
+    specs.RAM.update_info()
+    print("Gathering CPU information...")
+    specs.CPU.update_info()
+    print("Gathering disk information...")
+    specs.Disk.update_info()
+
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
 import pygame
 
-DISPATCHER = dict()
-THREADS = list()
-TOTAL_THREADS = 0
+class DISPATCHER: pass
 
 if "HEAT2D_DISABLE_ANSI_COLORS" in os.environ: __USE_ANSI_COLORS = False
 else: __USE_ANSI_COLORS = True
 
-from heat2d.engine import Engine
-from heat2d.stage import Stage
-from heat2d.gameobject import GameObject
-from heat2d.sprite import Sprite
-from heat2d import visuals
-from heat2d.timer import Timer, TickTimer
-from heat2d import ui
-from heat2d import postprocess
-from heat2d import networking
+from heat2d.libs.terminal import REVERSE, RESET, FG, clear
 
-from heat2d.libs.terminal import REVERSE, RESET, FG
+clear()
+
+from heat2d.version import *
+
+from heat2d.libs.color import Color, Palette
+from heat2d.engine import     Engine
+from heat2d.stage import      Stage
+from heat2d.gameobject import GameObject
+from heat2d.sprite import     Sprite
+from heat2d.timer import      Timer
+from heat2d import            audio
+from heat2d import            ui
+from heat2d import            gl
+from heat2d import            networking
+from heat2d.math import       Vector2
+
 if "HEAT2D_HIDE_WELCOME_MESSAGE" not in os.environ:
     if __USE_ANSI_COLORS:
-        print(f"\u2588{REVERSE}" + "\u2584"*20 + f"{RESET}\u2588")
-        print(f"\u2588{FG.lightred}       HEAT2D       {RESET}\u2588 Version : {VERSION}")
-        print(f"\u2588{FG.orange}    GAME ENGINE     {RESET}\u2588 State   : {VERSION_STATE.capitalize()}")
+        print(f"\u2588" + "\u2580"*20 + f"\u2588")
+        print(f"\u2588{FG.lightred}       HEAT2D       {RESET}\u2588 Version : {HEAT2D_VERSION}")
+        print(f"\u2588{FG.orange}    GAME ENGINE     {RESET}\u2588 State   : {HEAT2D_VERSION_STATE.capitalize()}")
         print("\u2588" + "\u2584"*20 + "\u2588\n")
     else:
-        print(f"\u2588{REVERSE}" + "\u2584"*20 + f"{RESET}\u2588")
-        print(f"\u2588       HEAT2D       \u2588 Version : {VERSION}")
-        print(f"\u2588    GAME ENGINE     \u2588 State   : {VERSION_STATE.capitalize()}")
+        print(f"\u2588" + "\u2580"*20 + f"\u2588")
+        print(f"\u2588       HEAT2D       \u2588 Version : {HEAT2D_VERSION}")
+        print(f"\u2588    GAME ENGINE     \u2588 State   : {HEAT2D_VERSION_STATE.capitalize()}")
         print("\u2588" + "\u2584"*20 + "\u2588\n")
 
-del sys, os, pygame, libs, engine, errors, window, gameobject, renderer, sprite, stage, timer, REVERSE, RESET, FG
+del os, pygame, libs, engine, errors, window, gameobject, renderer, sprite, stage, timer, math, REVERSE, RESET, FG
